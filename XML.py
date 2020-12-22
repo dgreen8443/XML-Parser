3b3d24f0202a0fd4933c42da8aea8f05c638b520
dgreen8443 <44809262+dgreen8443@users.noreply.github.com>
2020-12-22 20:29:21 +0000

    updated .py

--- XML.py
+++ XML.py
@@ -8,3 +8,8 @@ tree2 = ET.parse(sys.argv[2])
 root1 = tree1.getroot()
 root2 = tree2.getroot()
 
+# At the very lowest level, dictionaries
+# Create a list of lists
+# 
+for child in root1:
+	print(child.tag, child.attrib)
