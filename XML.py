ee3eeea5115201f290150b0148707259ff2b7ead
dgreen8443 <44809262+dgreen8443@users.noreply.github.com>
2020-12-22 21:13:04 +0000

    creating lists of dictionaries, need to also account fo attributes

--- XML.py
+++ XML.py
@@ -9,7 +9,28 @@ root1 = tree1.getroot()
 root2 = tree2.getroot()
 
 # At the very lowest level, dictionaries
-# Create a list of lists
+# Create a master list with list if attribute has children, dictionary otherwise
 # 
-for child in root1:
-	print(child.tag, child.attrib)
+list1 = []
+list2 = []
+def depthCheck(attribute):
+	if len(attribute) > 1:
+		return 1
+	else: 
+		return 0
+
+def rooting(attribute, lista):
+	for child in attribute:
+		if depthCheck(child) == 1:
+			innerlist = []
+			rooting(child, innerlist)
+			lista.append(innerlist)
+		else:
+			tag = child.tag
+			value = attribute.find(tag).text
+			temp_dict = {tag : value}
+			lista.append(temp_dict)
+		
+rooting(root1, list1)
+print(list1)
+print(list2)
